57f06f4bf311610759b197ec2ebe5657fcf8175e
cconst <4udconst@gmail.com>
2015-02-07 13:05:38 +0600

    DB: migrate from h2 to hsqldb

--- src/main/java/ru/nstu/cs/dss/WebApplicationConfiguration.java
+++ src/main/java/ru/nstu/cs/dss/WebApplicationConfiguration.java
@@ -1,8 +1,5 @@
 package ru.nstu.cs.dss;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,13 +12,5 @@ public class WebApplicationConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
-	}
-
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-		registration.addUrlMappings("/console/*");
-
-		return registration;
 	}
 }
